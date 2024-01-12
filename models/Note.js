5f347040461138670cbf32b92c17c221d598585b
DiabolicAugust <griffliway@gmail.com>
2024-01-12 19:10:02 +0200

    First commit with notes and todos started

--- models/Note.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const Note = new Schema({
-  text: { type: String, require: true },
-  dateOfCreation: { type: String, default: Date.now() },
-});
-
-export default model('Note', Note);
--- notes/notesRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import notesController from './notesController.js';
-
-const notesRouter = Router();
-
-notesRouter.post('/createNote', notesController.createNote);
-notesRouter.get('/getNotes', notesController.getNotes);
-
-export default notesRouter;
